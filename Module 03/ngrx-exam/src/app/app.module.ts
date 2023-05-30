11a5dc6853025ed280086b54d5274bdb157474e6
Kobi Hari <kobi2294@yahoo.com>
2023-05-30 10:41:28 +0300

    imported the store and store devtools into the application

--- Module 03/ngrx-exam/src/app/app.module.ts
+++ Module 03/ngrx-exam/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    StoreModule.forRoot(), 
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
